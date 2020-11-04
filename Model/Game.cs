19b7ca1108bbf562f9894466a46cee767f868622
Demonick2010 <kolyan20022002@gmail.com>
2020-11-04 14:19:43 +0200

    Add project files.

---
+++ Model/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GameIndustry.Model
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
