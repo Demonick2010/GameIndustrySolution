0c77368e2dc31c7b3ec62473247baa3018792b58
Demonick2010 <kolyan20022002@gmail.com>
2021-02-21 00:47:36 +0200

    Lesson 8. Module 1 final, Timer and IDisposible

---
+++ Helpers/GameHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameIndustry.Helpers
+{
+    public class GameHandler
+    {
+        // Создаём свойство элементов игры
+        public RSPOptions Choose { get; set; }
+        // Создаём свойство хранения элемента для победы
+        public RSPOptions WinCondition { get; set; }
+        // Создаём свойство хранения элемента для поражения
+        public RSPOptions LooseCondition { get; set; }
+        // Создаём свойство для хранения изображения
+        public string Image { get; set; }
+
+        // Создаём метод обработки результата игры
+        public GameState GameResult(GameHandler opponent)
+        {
+            if (Choose == opponent.Choose)
+                return GameState.Draw;
+            if (LooseCondition == opponent.Choose)
+                return GameState.Loss;
+            return GameState.Victory;
+        }
+    }
+    // Next step 6
+}
---
+++ Helpers/RSPOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameIndustry.Helpers
+{
+    public enum RSPOptions
+    {
+        Rock,
+        Scissors,
+        Paper
+    }
+}
